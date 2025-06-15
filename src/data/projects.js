b48de373b8bd15f0e7f047a7c851c6d619ec37d2
Alejandro Carrasco Aragón <72455516+OhhTuRnz@users.noreply.github.com>
2025-06-15 19:41:03 +0200

    Revert to YouTube Videos and Fix Embedding

--- src/data/projects.js
+++ src/data/projects.js
@@ -36,14 +36,14 @@ export const projects = [
     team: ["Alejandro Carrasco"],
     media: {
       type: "video",
-      url: "https://drive.google.com/file/d/1WjuRngFochZxm-BSDGQlWHCNIpoHoKbP/preview",
+      url: "https://www.youtube.com/watch?v=ioy0TIyqa8w",
       poster: "https://images.pexels.com/photos/586063/pexels-photo-586063.jpeg?auto=compress&cs=tinysrgb&w=800",
       thumbnail: "https://images.pexels.com/photos/586063/pexels-photo-586063.jpeg?auto=compress&cs=tinysrgb&w=400"
     },
     links: {
       github: "https://github.com/acarrasco/kspdg-2024",
       paper: "https://scholar.google.com/citations?view_op=view_citation&hl=en&user=6KamS70AAAAJ&citation_for_view=6KamS70AAAAJ:9yKSN-GCB0IC",
-      demo: "https://drive.google.com/file/d/1WjuRngFochZxm-BSDGQlWHCNIpoHoKbP/view?usp=sharing"
+      demo: "https://www.youtube.com/watch?v=ioy0TIyqa8w"
     },
     featured: true
   },
@@ -59,13 +59,13 @@ export const projects = [
     team: ["Alejandro Carrasco"],
     media: {
       type: "video",
-      url: "https://drive.google.com/file/d/10Ns7dQS1RJU6DxbfgDzwerZBaKFVbnQ7/preview",
+      url: "https://www.youtube.com/watch?v=qK2dbhiIw8c",
       poster: "https://images.pexels.com/photos/8386440/pexels-photo-8386440.jpeg?auto=compress&cs=tinysrgb&w=800",
       thumbnail: "https://images.pexels.com/photos/8386440/pexels-photo-8386440.jpeg?auto=compress&cs=tinysrgb&w=400"
     },
     links: {
       github: "https://github.com/acarrasco/kspdg-2025",
-      demo: "https://drive.google.com/file/d/10Ns7dQS1RJU6DxbfgDzwerZBaKFVbnQ7/view?usp=sharing"
+      demo: "https://www.youtube.com/watch?v=qK2dbhiIw8c"
     },
     featured: true
   },
